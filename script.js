eb55c3d0cc28c5d4b8a490d4d97501123098afeb
Lubomir Lenard <40657698+lubomir1618@users.noreply.github.com>
2022-10-03 22:19:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+// ui shortener
+document.addEventListener('DOMContentLoaded', () => {
+
+  const BASE_URL = 'https://shortener-infobip.herokuapp.com/';
+  const address = document.querySelector('#address');
+  const shorten = document.querySelector('#shorten');
+  const info = document.querySelector('#info');
+  const wrong = 'Something went wrong!';
+
+  shorten.addEventListener('click', () => {
+    if (isValidUrl(address.value)) {
+      getUniqueString(address.value);
+    }
+  });
+
+  function getUniqueString(addressToShorten) {
+    const options = { 
+      method: 'POST', body: `'{"url": "${addressToShorten}"}'`,
+      // mode: 'cors',
+      //headers : { 'Content-type' : 'application/json', Accept : 'application/json'}
+    };
+
+    fetch(BASE_URL + 'generate', options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        if (response.shortUrl) {
+          info.innerHTML = `Your shortened address for <a href="${response.originalUrl}">${response.originalUrl}</a> is 
+          <a href="${BASE_URL}${response.shortUrl}" target="_blank" title="shortened address">${BASE_URL}${response.shortUrl}</a> and it is valid till ${new Date(response.expirationDate)}`;          
+        } else {
+          info.innerHTML = wrong
+        }
+        address.value = '';
+      })
+      .catch(err => {
+        console.error(err);
+        info.innerHTML = wrong
+      }); 
+  }
+
+  function isValidUrl(address) {
+    try {
+      new URL(address);
+      return true;
+    } catch (err) {
+      info.innerHTML = 'Please add valid address.'
+      return false;
+    }
+  }
+});// ui shortener
 document.addEventListener('DOMContentLoaded', () => {
 
   const BASE_URL = 'https://shortener-infobip.herokuapp.com/';
@@ -13,7 +63,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function getUniqueString(addressToShorten) {
     const options = { 
-      method: 'POST', body: `'{"url": "${addressToShorten}"}'`
+      method: 'POST', body: `'{"url": "${addressToShorten}"}'`,
+      headers : { 'Content-type' : 'application/json', Accept : 'application/json'}
     };
 
     fetch(BASE_URL + 'generate', options)

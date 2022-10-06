602d366cd7097858269473bfe9d94dc003906f90
Lubomir Lenard <40657698+lubomir1618@users.noreply.github.com>
2022-10-06 12:15:42 +0200

    files updated final

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const BASE_URL = 'https://shortener-infobip.herokuapp.com/';
   const address = document.querySelector('#address');
   const shorten = document.querySelector('#shorten');
+  const loading = document.querySelector('#loading');
   const info = document.querySelector('#info');
   const wrong = 'Something went wrong!';
 
   shorten.addEventListener('click', () => {
     if (isValidUrl(address.value)) {
       getUniqueString(address.value);
+      loading.style.display = 'block';
     } else {
       info.innerHTML = 'Please add valid address.'
     }
@@ -28,17 +30,18 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(BASE_URL + 'generate', options)
       .then(response => response.json())
       .then(response => {
-        console.log(response);
         if (response.shortUrl) {
-          info.innerHTML = `Your shortened address for <a href="${response.originalUrl}" target="_blank">${response.originalUrl}</a> is 
-          <a href="${BASE_URL}${response.shortUrl}" target="_blank" title="shortened address">${BASE_URL}${response.shortUrl}</a> and it is valid till ${new Date(response.expirationDate)}`;          
+          info.innerHTML = `Your shortened address: <br>
+          <a href="${BASE_URL}${response.shortUrl}" target="_blank" title="shortened address">${BASE_URL}${response.shortUrl}</a> <br>
+          <p>valid till ${new Date(response.expirationDate)}<p>`;
+          loading.style.display = 'none';
         } else {
-          info.innerHTML = wrong
+          info.innerHTML = wrong;
+          loading.style.display = 'none';
         }
         address.value = '';
       })
       .catch(err => {
-        console.error(err);
         info.innerHTML = wrong
       }); 
   }
